e94e3f79eaa434d08f81563e5ed959b15a2c92fa
Jian, Jia-Xian <60592114+chia-shein@users.noreply.github.com>
2024-12-12 11:26:05 +0800

    Update mhd2nrrd.py

--- Data_processing/mhd2nrrd.py
+++ Data_processing/mhd2nrrd.py
@@ -1,15 +1,26 @@
-import os
 import SimpleITK as sitk
+import numpy as np
+import os
+import glob
+
 input_dir = '/work/u5453836/LUNA_Data'
 output_dir = '/work/u5453836/LUNA_Data_nrrd'
 os.makedirs(output_dir, exist_ok=True)
 
+for file_path in glob.glob(os.path.join(input_dir, '*.mhd')):
+    input_image = sitk.ReadImage(file_path)
+    image_array = sitk.GetArrayFromImage(input_image)
 
-for filename in os.listdir(input_dir):
-    if filename.endswith('.mhd'):
-        input_path = os.path.join(input_dir, filename)
-        input_image = sitk.ReadImage(input_path)
-        output_filename = os.path.splitext(filename)[0] + '.nrrd'
-        output_path = os.path.join(output_dir, output_filename)
-        sitk.WriteImage(input_image, output_path)
-print("Conversion completed.")
+    # data.min() and data.max() constraint
+    current_min = image_array.min()
+    current_max = image_array.max()
+    rescaled_array = (image_array - current_min) / (current_max - current_min) * (3071 + 1000) - 1000
+
+    # data type constraint
+    int16_array = rescaled_array.astype(np.int16)
+    rescaled_image = sitk.GetImageFromArray(int16_array)
+    rescaled_image.CopyInformation(input_image)
+    
+    file_name = os.path.basename(file_path).replace('.mhd', '.nrrd')
+    output_file = os.path.join(output_dir, file_name)
+    sitk.WriteImage(rescaled_image, output_file)
